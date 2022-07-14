a9aaaee4824ef41c6557ff25872bd9a2d4fe823d
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-07-14 23:42:43 +0300

    dropdown fixed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { log } from 'console'
 import React, { useEffect, useState } from 'react'
 import { useDebounce } from '../hooks/debounce'
 import { useSearchUsersQuery } from '../store/github/gitgub.api'
@@ -6,13 +5,18 @@ import { useSearchUsersQuery } from '../store/github/gitgub.api'
 
 const HomePage = () => {
   const [search, setSearch] = useState('')
-  const debouced =  useDebounce(search)
-  const { isLoading, isError, data } = useSearchUsersQuery('maksym')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced =  useDebounce(search)
+  const { isLoading, isError, data: users } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3
+  })
+
+  // console.log(data);
+  
 
   useEffect(() => {
-    console.log(debouced);
-    
-  }, [debouced])
+    setDropdown(debounced.length > 3 && users?.length! > 0)
+  }, [debounced, users])
   
   return (
     <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -27,9 +31,17 @@ const HomePage = () => {
           onChange={e => setSearch(e.target.value)}
         />
 
-        <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, atque!
-        </div>
+        {dropdown &&
+          <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll'>
+            {isLoading && <p className='text-center'>Loading...</p>}
+            {users?.map(u => (
+              <li key={u.id}
+                className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+              >
+                {u.login}</li>
+            ))}
+          </ul>
+        }
       </div>
     </div>
   )
