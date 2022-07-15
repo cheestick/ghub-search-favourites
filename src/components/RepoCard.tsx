5eac14b20c1b5805a969882ce32c18c0b9e1d9fb
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-07-15 08:41:12 +0300

    add remove button added and refactored.fav page filled out. local storage saved

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useActions } from '../hooks/actions'
+import { useAppSelector } from '../hooks/redux'
 import { IRepo } from '../models/models'
 
-const RepoCard = ({repos}: {repos: IRepo}) => {
+const RepoCard = ({ repos }: { repos: IRepo }) => {
+  const { addFavourite, removeFavourite } = useActions()
+  const { favourites } = useAppSelector(state => state.github)
+
+  const [isFav, setIsFav] = useState(favourites.includes(repos.html_url))
+
+  const addToFavourites = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    addFavourite(repos.html_url)
+    setIsFav(true)
+  }
+
+  const removeFromFavourites = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event?.preventDefault()
+    removeFavourite(repos.html_url)
+    setIsFav(false)
+  }
+
   return (
       <div className='border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transtion-all'>
         <a href={repos.html_url} target='_blank' rel="noreferrer">
@@ -10,7 +29,23 @@ const RepoCard = ({repos}: {repos: IRepo}) => {
                   Forks: <span className='font-bold mr-2'>{repos.forks}</span>
                   Watchers: <span className='font-bold'>{repos.watchers}</span>
               </p>
-              <p className='text-sm font-thin'>{repos?.description}</p>
+        <p className='text-sm font-thin'>{repos?.description}</p>
+        
+        {!isFav &&
+          <button
+            className='py-2 px-4 bg-yellow-400 mr-2 rounded hover:shadow-md transition-all'
+            onClick={addToFavourites}>
+            Add
+          </button>
+        }
+
+        {isFav &&
+          <button
+            className='py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all'
+            onClick={removeFromFavourites}>
+            Remove
+          </button>
+        }
         </a >
     </div>
   )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore} from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { githubApi } from "./github/gitgub.api";
+import { githubReducer } from "./github/github.slice";
 
 export const store = configureStore({
     reducer: {
         [githubApi.reducerPath]: githubApi.reducer,
+        github: githubReducer
     },
     middleware: getDefaultMiddleware => [...getDefaultMiddleware(), githubApi.middleware]
 })
 
 setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
